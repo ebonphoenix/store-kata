2e503f091fef750d8c80dd187893817e8b3e430f
Shayne Vacher <ebonphoenix@hotmail.com>
2021-06-29 10:59:18 -0700

    ensured that tests called code under test... should be ready to start developing

--- spec/spec.js
+++ spec/spec.js
@@ -2,4 +2,7 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+  it("should call the code under test", function() {
+	  expect(helloWorld).toEqual("Hello World");
+  });
 });
